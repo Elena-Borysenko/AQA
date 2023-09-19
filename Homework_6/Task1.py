b46addf18c92d145fa8134a5cd5ba587af46d508
Elena-Borysenko <108331808+Elena-Borysenko@users.noreply.github.com>
2023-09-19 11:27:15 +0300

    chahge

--- Homework_6/Task1.py
+++ Homework_6/Task1.py
@@ -8,14 +8,14 @@ with open('test_file.csv', 'r') as usd_salary_file, open('salaries_uah.csv', 'w'
 
     header = next(usd_reader)
     header.append('Total_Salary_UAH')
-    uah_writer.writerow(header[0:2] + ['Total_Salary_UAH'] + header[2:])
+    uah_writer.writerow(header)
 
     for row in usd_reader:
           employee_name = row[0]
           usd_salaries = list(map(float, row[1:]))
           uah_salaries = [usd_salary * usd_to_uah_rate for usd_salary in usd_salaries]
           total_uah_salary = sum(uah_salaries)
-          uah_writer.writerow([employee_name] + usd_salaries + [total_uah_salary])
+          uah_writer.writerow([employee_name] + uah_salaries + [total_uah_salary])
 
 
 
